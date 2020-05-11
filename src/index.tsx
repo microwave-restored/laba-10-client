1fe6e3cc693735a92799b6bcaf72e61155cc2bc3
microwave-restored <daily.mai@yandex.ru>
2020-05-11 13:33:02 +0300

    v1

---
+++ src/index.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider, useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+import Pages from './pages';
+import Login from './pages/login';
+import { resolvers, typeDefs } from './resolvers';
+import injectStyles from './styles';
+
+const cache = new InMemoryCache();
+
+const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
+  cache,
+  link: new HttpLink({
+  uri: 'https://maxgraphql.herokuapp.com/',
+  headers: {
+      authorization: localStorage.getItem('token'),
+      'client-name': 'Space Explorer [web]',
+      'client-version': '1.0.0',
+    },
+  }),
+  resolvers,
+  typeDefs,
+});
+
+
+
+cache.writeData({
+  data: {
+    isLoggedIn: !!localStorage.getItem('token'),
+    cartItems: [],
+  },
+});
+
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
+
+injectStyles();
+ReactDOM.render(
+
+  <ApolloProvider client={client}>
+    <IsLoggedIn />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
